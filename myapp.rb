f89176327d1fa7b40286af791531dbeb1c36bf8f
YegorSSS <yegorsss@yandex.ru>
2014-10-28 16:51:48 +0200

    Mongo, first commit

--- myapp.rb
+++ myapp.rb
@@ -1,19 +1,40 @@
 require 'sinatra'
+require 'mongoid'
+require 'json/ext'
 
-get '/' do
-	'<p><a href="/memory">Memory</a></p>
-	<p><a href="/disk">Disk</a></p>'
+
+configure do
+  Mongoid.load!("mongoid.yml")
 end
 
-
-get '/memory' do
-	m = `free -m` 
-	"<pre>#{m}</pre>"
-  
+class Statistic
+	include Mongoid::Document
+	field :time, type: String
+	field :info, type: String
 end
 
-get '/disk' do
-	d = `df -h`
-	"<pre>#{d}</pre>"
+get "/" do
+	@stat = Statistic.all
+	erb :home
 end
 
+post "/" do
+	static = Statistic.new(time: params[:time], info: params[:info])
+	static.save
+	redirect "/"
+end
+
+get "/memory" do
+	@m = `free -m`
+	erb :memory
+end
+
+get "/disk" do
+	@h = `df -h`
+	erb :disk
+end
+
+post "/delete" do
+	Statistic.where(time: params[:time]).delete
+	redirect "/"
+end
